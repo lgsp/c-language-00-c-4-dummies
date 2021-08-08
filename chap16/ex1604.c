6540271ca02c010b79e7dff9235f8ec36c6a88f5
lgsp <laurent.garnier.superprof@gmail.com>
2021-08-08 17:11:06 +0200

    Finish chapter 16

--- chap16/ex1604.c
+++ chap16/ex1604.c
@@ -3,26 +3,35 @@
 
 int main()
 {
-  struct date
+  typedef struct date
   {
     int month;
     int day;
     int year;
-  };
+  } calendar;
+  
+  typedef struct id
+  {
+    char first[25];
+    char last[25];
+  } personal;
+  
   struct human
   {
-    char name[45];
-    struct date birthday;
+    personal name;
+    calendar birthday;
   };
   struct human president;
 
-  strcpy(president.name, "George Washington");
+  strcpy(president.name.first, "George");
+  strcpy(president.name.last, "Washington");
   president.birthday.month = 2;
   president.birthday.day = 22;
   president.birthday.year = 1732;
 
-  printf("%s was born on %d/%d/%d\n",
-	 president.name,
+  printf("%s %s was born on %d/%d/%d\n",
+	 president.name.first,
+	 president.name.last,
 	 president.birthday.month,
 	 president.birthday.day,
 	 president.birthday.year);
